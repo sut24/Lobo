bf89118e3fa5329b435822a2b1ba119644bb54c9
sut24 <sut24@web.de>
2019-02-10 19:06:38 +0100

    inital commit of all files

---
+++ myStuff.js
@@ -0,0 +1,7 @@
+  module.exports = {
+    foo: () => 
+    { 
+      var myImageUrl = "https://www.google.de/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+      return myImageUrl;
+    }
+  }
